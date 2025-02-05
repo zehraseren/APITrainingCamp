37657c27a30d798eaf9bf0c5d802e848aa7c8ab8
Zehra Seren <fatmazehraseren@gmail.com>
2025-02-05 21:43:53 +0300

    Lecture 4 is completed

---
+++ ApiProjectCamp/Entities/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjectCamp.Entities
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string MapLocation { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string OpenHours { get; set; }
+    }
+}
---
+++ ApiProjectCamp/Entities/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProjectCamp.Entities
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
