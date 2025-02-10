a5a18d364874bbcc60e6e4007ee904ff75985741
Zehra Seren <fatmazehraseren@gmail.com>
2025-02-11 00:14:40 +0300

    Lecture 8 is completed

---
+++ ApiProjectCamp/Controllers/ChefsController.cs
@@ -0,0 +1,57 @@
+﻿using ApiProjectCamp.Context;
+using ApiProjectCamp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ChefList()
+        {
+            var values = _context.Chefs.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateChef(Chef chef)
+        {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return Ok("Şef sisteme başarıyla eklendi.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteChef(int id)
+        {
+            var value = _context.Chefs.Find(id);
+            _context.Chefs.Remove(value);
+            _context.SaveChanges();
+            return Ok("Şef sistemden başarıyla silindi.");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetChef(int id)
+        {
+            var value = _context.Chefs.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateChef(Chef chef)
+        {
+            _context.Chefs.Update(chef);
+            _context.SaveChanges();
+            return Ok("Şef sistemde başarıyla güncellendi.");
+        }
+    }
+}
