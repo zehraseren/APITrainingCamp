c91c1b05daebef743864f5bef43daa5d9c4fec4c
Zehra Seren <fatmazehraseren@gmail.com>
2025-02-11 00:05:43 +0300

    Lecture 7 is completed

--- ApiProjectCamp/Controllers/CategoriesController.cs
+++ ApiProjectCamp/Controllers/CategoriesController.cs
@@ -15,12 +15,43 @@ namespace ApiProjectCamp.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
+
         [HttpPost]
         public IActionResult CreateCategory(Category category)
         {
             _context.Categories.Add(category);
             _context.SaveChanges();
-            return Ok("Kategori işlemi başarılı.");
+            return Ok("Kategori ekleme işlemi başarılı.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori silme işlemi başarılı.");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori güncellem işlemi başarılı.");
         }
     }
 }
