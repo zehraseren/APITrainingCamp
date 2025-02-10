77db29cc3872bd76f2b06fb18bdf9c2b86c9a62c
Zehra Seren <fatmazehraseren@gmail.com>
2025-02-10 23:43:42 +0300

    Lecture 6 is completed

---
+++ ApiProjectCamp/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using ApiProjectCamp.Context;
+using ApiProjectCamp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori işlemi başarılı.");
+        }
+    }
+}
--- ApiProjectCamp/Program.cs
+++ ApiProjectCamp/Program.cs
@@ -1,6 +1,10 @@
+using ApiProjectCamp.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApiContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
