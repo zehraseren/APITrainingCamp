9ba1ef77eee0e37145ad43e6e4a7319d5164c20c
Zehra Seren <fatmazehraseren@gmail.com>
2025-02-21 21:05:03 +0300

    Lecture 12 is completed

---
+++ ApiProjectCamp/Controllers/FeaturesController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using ApiProjectCamp.Context;
+using ApiProjectCamp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using ApiProjectCamp.Dtos.FeatureDtos;
+
+namespace ApiProjectCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ApiContext _context;
+
+        public FeaturesController(IMapper mapper, ApiContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult FeatureList()
+        {
+            var features = _context.Features.ToList();
+            return Ok(_mapper.Map<List<ResultFeatureDto>>(features));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetList()
+        {
+            var feature = _context.Features.ToList();
+            return Ok(_mapper.Map<List<GetByIdFeatureDto>>(feature));
+        }
+
+        [HttpPost]
+        public IActionResult CreateFeature(CreateFeatureDto cfdto)
+        {
+            var feature = _mapper.Map<Feature>(cfdto);
+            _context.Features.Add(feature);
+            return Ok("Ekleme işlemi başarılı.");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteFeature(int id)
+        {
+            var feature = _context.Features.Find(id);
+            _context.Features.Remove(feature);
+            return Ok("Silme işlemi başarılı");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateFeature(UpdateFeatureDto ufdto)
+        {
+            var feature = _mapper.Map<Feature>(ufdto);
+            _context.Features.Update(feature);
+            return Ok("Güncelleme işlemi başarılı.");
+        }
+    }
+}
