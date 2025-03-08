2af521a516b9fa939abe3cef18706a47e12a8eac
Zehra Seren <fatmazehraseren@gmail.com>
2025-03-08 16:12:22 +0300

    Lecture 14 is completed

---
+++ ApiProjectCamp/ValidationRules/ProductValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using ApiProjectCamp.Entities;
+
+namespace ApiProjectCamp.ValidationRules
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Ürün adını boş geçmeyiniz.").MinimumLength(2).WithMessage("En az iki karakter girişi yapınız.").MaximumLength(50).WithMessage("En fazla 50 karakter girişi yapınız."); ;
+
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Ürün fiyatı boş geçilemez.").LessThan(0).WithMessage("Fiyatı 0'dan büyük giriniz.").GreaterThan(1000).WithMessage("Ürün fiyatı bu kadar yüksek olamaz, girdiğiniz değeri kontrol ediniz!");
+
+            RuleFor(x => x.ProductDescription).NotEmpty().WithMessage("Ürün açıklaması boş geçilemez.");
+        }
+    }
+}
