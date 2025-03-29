0c9fe00b0353070ae5e80571eede76847c9a4f0e
Zehra Seren <fatmazehraseren@gmail.com>
2025-03-29 03:57:33 +0300

    Lecture 19 is completed

---
+++ ApiProjectCamp.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebUI.Controllers;
+
+public class DefaultController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ ApiProjectCamp.WebUI/ViewComponents/_HeadComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebUI.ViewComponents;
+
+public class _HeadComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
