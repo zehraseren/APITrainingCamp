bce0a8ddebc984b5ce75104c94c9dba23924618e
Zehra Seren <fatmazehraseren@gmail.com>
2025-04-09 22:11:01 +0300

    Lecture 23 is completed

--- ApiProjectCamp.WebUI/ViewComponents/DefaultViewComponents/_ServiceComponentPartial.cs
+++ ApiProjectCamp.WebUI/ViewComponents/DefaultViewComponents/_ServiceComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using ApiProjectCamp.WebUI.Dtos.ServiceDtos;
 
 namespace ApiProjectCamp.WebUI.ViewComponents.DefaultViewComponents;
 
 public class _ServiceComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _ServiceComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var response = await client.GetAsync("https://localhost:44392/api/Services");
+        if (response.IsSuccessStatusCode)
+        {
+            var data = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<ResultServiceDto>>(data);
+            return View(result);
+        }
         return View();
     }
 }
