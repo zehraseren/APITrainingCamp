6053f2168315dc8ebd5917436fbd12f3631dcf80
Zehra Seren <fatmazehraseren@gmail.com>
2025-04-23 00:52:59 +0300

    Lecture 25 is completed

---
+++ ApiProjectCamp.WebUI/Dtos/ProductDtos/ResultProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjectCamp.WebUI.Dtos.ProductDtos;
+
+public class ResultProductDto
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+    public int CategoryId { get; set; }
+}
---
+++ ApiProjectCamp.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuProductComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using ApiProjectCamp.WebUI.Dtos.ProductDtos;
+
+namespace ApiProjectCamp.WebUI.ViewComponents.DefaultMenuViewComponents;
+
+public class _DefaultMenuProductComponentPartial : ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _DefaultMenuProductComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var response = await client.GetAsync("https://localhost:44392/api/Products");
+        if (response.IsSuccessStatusCode)
+        {
+            var data = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<ResultProductDto>>(data);
+            return View(result);
+        }
+        return View();
+    }
+}
