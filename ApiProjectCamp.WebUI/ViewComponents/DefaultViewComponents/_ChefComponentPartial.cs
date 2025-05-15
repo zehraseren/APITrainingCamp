8f33947cce0a5420ca80e9a1671ee644008622e9
Zehra Seren <fatmazehraseren@gmail.com>
2025-05-15 20:19:28 +0300

    Lecture 30 is completed

---
+++ ApiProjectCamp.WebUI/ViewComponents/DefaultViewComponents/_ChefComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using ApiProjectCamp.WebUI.Dtos.ChefDtos;
+
+namespace ApiProjectCamp.WebUI.ViewComponents.DefaultViewComponents;
+
+public class _ChefComponentPartial : ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _ChefComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var response = await client.GetAsync("https://localhost:44392/api/Chefs");
+        if (response.IsSuccessStatusCode)
+        {
+            var data = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<ResultChefDto>>(data);
+            return View(result);
+        }
+        return View();
+    }
+}
