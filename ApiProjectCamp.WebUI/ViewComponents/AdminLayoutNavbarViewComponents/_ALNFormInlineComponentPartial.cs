29a610f845090aab870fef4c41d46254c5249265
Zehra Seren <fatmazehraseren@gmail.com>
2025-05-29 21:43:43 +0300

    Lecture 32 is completed

---
+++ ApiProjectCamp.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_ALNFormInlineComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebUI.ViewComponents.AdminLayoutNavbarViewComponents;
+
+public class _ALNFormInlineComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ ApiProjectCamp.WebUI/ViewComponents/AdminLayoutViewComponents/_ALNavbarComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebUI.ViewComponents.AdminLayoutViewComponents;
+
+public class _ALNavbarComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
