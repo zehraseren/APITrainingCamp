967de6e49043e3466b04be743b45c7b1cc1baff0
Zehra Seren <fatmazehraseren@gmail.com>
2025-08-04 21:37:50 +0300

    Lecture 55 is completed

---
+++ ApiProjectCamp.WebUI/Controllers/AIController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebUI.Controllers;
+public class AIController : Controller
+{
+    public IActionResult CreateRecipe()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CreateRecipeWithOpenAI(string prompt)
+    {
+        var apiKey = "YOUR_API_KEY_HERE";
+        return View();
+    }
+}
